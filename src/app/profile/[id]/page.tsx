d9f01e427ca5bfdfb11d5b091b972464c1129542
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-08-03 01:55:27 +0700

    configure profile page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,25 @@
+import ProfilePage from "@/components/ProfilePage";
+import { getUserProjects } from "@/lib/actions"
+import { UserProfile } from "@/types";
+
+type Props = {
+  params: {
+    id: string;
+  }
+}
+
+const UserProfile = async ({ params : { id }}: Props) => {
+  const result = await getUserProjects(id, 100) as { user: UserProfile};
+
+  if (!result?.user) {
+    return (
+      <p className="no-result-text">Failed to fetch user info.</p>
+    )
+  }
+
+  return (
+    <ProfilePage user={result?.user} />
+  )
+}
+
+export default UserProfile
