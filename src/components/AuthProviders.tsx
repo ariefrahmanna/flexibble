dc3498d16151553f7bb32e9fdf5afc99b4cae24f
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-08-02 19:04:51 +0700

    Improve provider sign in button

--- src/components/AuthProviders.tsx
+++ src/components/AuthProviders.tsx
@@ -2,6 +2,7 @@
 
 import { getProviders, signIn } from 'next-auth/react';
 import { useState, useEffect } from 'react';
+import Btn from './Btn';
 
 type Provider = {
   id: string;
@@ -33,9 +34,11 @@ const AuthProviders = () => {
     return (
       <div>
         {Object.values(providers).map((provider: Provider, i) => (
-          <button key={i} onClick={() => signIn(provider?.id)}>
-            {provider?.id}
-          </button>
+          <Btn
+            title={provider?.id}
+            key={i}
+            handleClick={() => signIn(provider?.id)}
+          />
         ))}
       </div>
     );
