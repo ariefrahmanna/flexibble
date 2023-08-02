1edd984c97b388b011780cf0b74b63115ccee74b
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2023-08-03 01:47:51 +0700

    configure load more projects within home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Categories from '@/components/Categories';
+import LoadMore from '@/components/LoadMore';
 import ProjectCard from '@/components/ProjectCard';
 import { fetchAllProjects } from '@/lib/actions';
 import { ProjectInterface } from '@/types';
@@ -15,12 +16,21 @@ type ProjectSearch = {
   };
 };
 
-type Props = {
-  searchParams: { category?: string };
+type SearchParams = {
+  category?: string;
+  endCursor?: string;
 };
 
-const Home = async ({ searchParams: { category } }: Props) => {
-  const data = (await fetchAllProjects(category)) as ProjectSearch;
+type Props = {
+  searchParams: SearchParams;
+};
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endCursor } }: Props) => {
+  const data = (await fetchAllProjects(category, endCursor)) as ProjectSearch;
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -34,6 +44,8 @@ const Home = async ({ searchParams: { category } }: Props) => {
       </section>
     );
   }
+
+  const pagination = data?.projectSearch?.pageInfo;
 
   return (
     <section className="flex-start flex-col paddings mb-16">
@@ -53,7 +65,12 @@ const Home = async ({ searchParams: { category } }: Props) => {
         ))}
       </section>
 
-      <h1>LoadMore</h1>
+      <LoadMore
+        startCursor={pagination?.startCursor}
+        endCursor={pagination?.startCursor}
+        hasPreviousPage={pagination?.hasPreviousPage}
+        hasNextPage={pagination?.hasNextPage}
+      />
     </section>
   );
 };
